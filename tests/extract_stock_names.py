7d95106a2fcd2890e9c7cdf5b7d8b7287699ad7f
abhikr74 <ravi.abhishek74@hotmail.com>
2020-05-05 23:01:14 +0530

    added app_home

--- tests/extract_stock_names.py
+++ tests/extract_stock_names.py
@@ -1,3 +1,8 @@
 import pandas as pd
-top_1000_stocks_path="C:\Users\SCHOOL\PycharmProjects\stock_analyzer\data\top_1000_stocks.csv"
+from app_properties import ApplicationProperties as props
+import os
+
+top_1000_stocks_path = os.path.join(props.data_dir, "top_1000_stocks.csv")
 top_1000_stocks = pd.read_csv(top_1000_stocks_path)
+
+
